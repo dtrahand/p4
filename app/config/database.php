0d83874c95dc2f883c439fb6b745435edcf1fe45
Delphine <delphine_trahand@yahoo.com>
2014-11-21 10:34:17 +0100

    added password to database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'musicians',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'gYkYVvkD3p',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
