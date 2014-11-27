e79be78d314be6641767b90471df1f14309e3fec
Delphine <delphine_trahand@yahoo.com>
2014-11-27 22:41:03 +0100

    addedproduction/database.php

---
+++ app/config/production/database.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'connections' => array(
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'musicians',
+			'username'  => 'root',
+			'password'  => 'gYkYVvkD3p',
+		),
+	),
+);
